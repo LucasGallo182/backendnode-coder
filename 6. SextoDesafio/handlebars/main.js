2d53c26e964d339f17e86fc41ae3328e6cdc5889
Lucas Gallo <lucas.182g@gmail.com>
2022-11-10 22:06:06 -0300

    Desafio 6

--- 6. SextoDesafio/handlebars/main.js
+++ 6. SextoDesafio/handlebars/main.js
@@ -3,27 +3,27 @@ const express = require("express");
 const app = express();
 
 //Socket / Http
-const { Server } = require("socket.io");
-const http = require("http");
-const server = http.createServer(app);
-const io = new Server(server);
+const { Server } = require("socket.io")
+const http = require("http")
+const server = http.createServer(app)
+const io = new Server(server)
 
 //Container
-const Contenedor = require("./contenedor");
-const newProduct = new Contenedor("products.json");
-const newChat = new Contenedor("chat.json");
+const Contenedor = require("./contenedor")
+const newProduct = new Contenedor("products.json")
+const newChat = new Contenedor("chat.json")
 
 //Parse JSON / public
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
 //Handlebars
 const handlebars = require('express-handlebars')
 
 //Set app
-app.set("views", "./views"); //Especifica el directorio de vistas
-app.set("view engine", "hbs"); //Registra el motor de plantillas
+app.set("views", "./views") //Especifica el directorio de vistas
+app.set("view engine", "hbs") //Registra el motor de plantillas
 
 //Configuracion handlebars
 app.engine(
@@ -34,36 +34,36 @@ app.engine(
         layoutsDir: __dirname + "/views",
         partialsDir: __dirname + "/views/partials",
     })
-);
+)
 
 //Io Connection
 io.on("connection", async (socket) => {
     console.log("Usuario Conectado")
 
     const products = await newProduct.getAll();
-    socket.emit("list-welcome", products)
+    socket.emit("list-products", products)
 
     const msgs = await newChat.getAll();
-    socket.emit("list-msg-welcome", msgs)
+    socket.emit("send-message", msgs)
 
     socket.on("newMessage", async (data) => {
         await newChat.save(data)
 
         const msg = await newChat.getAll();
-        io.sockets.emit("list-msg-welcome-act", msg)
-    });
+        io.sockets.emit("send-message-update", msg)
+    })
 
     socket.on("productAdded", async (data) => {
         await newProduct.save(data);
 
         const products = await newProduct.getAll()
-        io.sockets.emit("list-act", products)
-    });
+        io.sockets.emit("list-products-update", products)
+    })
 
     socket.on("disconnect", () => {
         console.log("Usuario desconectado")
-    });
-});
+    })
+})
 
 //Endpoints************
 //GetList
@@ -87,7 +87,7 @@ app.post("/productos", async (req, res) => {
 //Server on
 const PORT = 8080;
 server.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
-});
+    console.log(`Server started at http://localhost:${PORT}`)
+})
 
-server.on("error", (err) => console.log(err));
+server.on("error", (err) => console.log(err))
